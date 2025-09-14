243632da597d42bc4d6373bb8b738debfbb442a4
Shubham Prasad Pednekar <shubhampednekar1999@gmail.com>
2025-09-13 21:42:33 -0500

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -237,22 +237,12 @@
     syntaxHighlighting.enable = true;
     histSize = 10000;
     promptInit = ''
-      # this act as your ~/.zshrc but for all users (/etc/zshrc)
       source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
       source /etc/powerlevel10k/p10k.zsh
 
-      # Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-      # Initialization code that may require console input (password prompts, [y/n]
-      # confirmations, etc.) must go above this block; everything else may go below.
-      # double single quotes ('') to escape the dollar char
       if [[ -r "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
         source "''${XDG_CACHE_HOME:-''$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
       fi
-
-      # uncomment if you want to customize your LS_COLORS
-      # https://manpages.ubuntu.com/manpages/plucky/en/man5/dir_colors.5.html
-      #LS_COLORS='...'
-      #export LS_COLORS
     '';
     ohMyZsh = {
       enable = true;
