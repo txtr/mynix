3335ac794ea298d841032253fb179c26bf17cea5
Shubham Prasad Pednekar <shubhampednekar1999@gmail.com>
2025-09-13 19:02:09 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,7 +5,12 @@ sudo nixos-rebuild switch
 sudo nix-collect-garbage -d
 sudo nix-store --optimise
 
+
+cp ./.zshrc ~/.zshrc
+cp ./.p10k.zsh ~/.p10k.zsh
+cp ./.alacritty.toml ~/.alacritty.toml
 sudo cp ./wallpaper.jpeg /wallpaper.jpeg
+
 dconf load / < ./dconf
 
 google-chrome-stable
@@ -15,16 +20,16 @@ echo "Close Firefox and Google Chrome. Press any key to continue..."
 read -n 1 -s
 echo "You pressed a key! Continuing..."
 
-cd ~/.mozilla/firefox/*.default
-sudo rm -rf ./* ./.*
-git init -b main
-git remote add origin "https://github.com/txtr/firefox_profile.git"
-git fetch
-git pull origin main
+# cd ~/.mozilla/firefox/*.default
+# sudo rm -rf ./* ./.*
+# git init -b main
+# git remote add origin "https://github.com/txtr/firefox_profile.git"
+# git fetch
+# git pull origin main
 
-cd ~/.config/google-chrome/Default
-rm -rf ./* ./.*
-git init -b main
-git remote add origin "https://github.com/txtr/chromium_profile.git"
-git fetch
-git pull origin main
+# cd ~/.config/google-chrome/Default
+# rm -rf ./* ./.*
+# git init -b main
+# git remote add origin "https://github.com/txtr/chromium_profile.git"
+# git fetch
+# git pull origin main
