7bc61013ded66e196c82084395d08ebc28fbab88
Shubham Prasad Pednekar <shubhampednekar1999@gmail.com>
2025-10-10 15:26:28 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+read -p "Enter hostname for this system: " new_hostname
+echo "networking.hostName = \"$new_hostname\";" >> configuration.nix
+
 sudo cp ./configuration.nix /etc/nixos/configuration.nix
 sudo nixos-rebuild switch
 sudo nix-collect-garbage -d
@@ -10,23 +13,3 @@ sudo cp ./wallpaper.jpeg /wallpaper.jpeg
 
 dconf load / < ./dconf
 
-# google-chrome-stable
-# firefox
-
-# echo "Close Firefox and Google Chrome. Press any key to continue..."
-# read -n 1 -s
-# echo "You pressed a key! Continuing..."
-
-# cd ~/.mozilla/firefox/*.default
-# sudo rm -rf ./* ./.*
-# git init -b main
-# git remote add origin "https://github.com/txtr/firefox_profile.git"
-# git fetch
-# git pull origin main
-
-# cd ~/.config/google-chrome/Default
-# rm -rf ./* ./.*
-# git init -b main
-# git remote add origin "https://github.com/txtr/chromium_profile.git"
-# git fetch
-# git pull origin main
