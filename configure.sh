f90c81d7a9c47ba2962d2030cf98823471509269
Shubham Prasad Pednekar <shubhampednekar1999@gmail.com>
2025-10-17 22:54:33 -0500

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
-# Exit immediately on error
-set -e
-
 echo "Checking internet connection..."
 if ping -q -c 1 -W 2 8.8.8.8 >/dev/null; then
     echo "Internet connection detected."
+    sudo cp ./configuration.nix /etc/nixos/configuration.nix
+    sudo nixos-rebuild switch
+    sudo nix-collect-garbage -d
+    sudo nix-store --optimise
 else
     echo "No internet connection. Exiting..."
     exit 1
 fi
-
-sudo cp ./configuration.nix /etc/nixos/configuration.nix
-sudo nixos-rebuild switch
-sudo nix-collect-garbage -d
-sudo nix-store --optimise
-
 
 sudo cp ./wallpaper.jpeg /wallpaper.jpeg
 
