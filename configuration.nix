90baec925cff5dbfab1f6e81a141ea042057f8a2
Shubham Prasad Pednekar <shubhampednekar1999@gmail.com>
2025-11-11 20:54:18 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,4 +4,27 @@
   imports = [
     ./base.nix
   ];
+
+  networking.hostName = "thor";
+
+  hardware.graphics.extraPackages =  with pkgs; [
+    rocmPackages.clr.icd # OpenCL
+    pkgs.amdvlk # Vulkan
+  ];
+  
+  hardware.graphics.extraPackages32 = [ pkgs.driversi686Linux.amdvlk ]; # Vulkan support for 32-bit applications
+
+  environment.variables.ROC_ENABLE_PRE_VEGA = "1"; # Reenable OpenCL on Polaris-based cards above ROCm 4.5
+  environment.variables.AMD_VULKAN_ICD = "RADV"; # Force RADV
+
+  services.xserver.videoDrivers = [
+    # Free AMD Video Drivers
+    "amdgpu"
+
+    # # Propreitary AMD Video Drivers
+    # "amdgpu-pro"
+  ];
+
+  virtualisation.docker.enable = true; # Install Docker
+  users.extraGroups.docker.members = [ "txtr" ]; # Allow user to access docker socket
 }
